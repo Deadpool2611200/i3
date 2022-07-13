5eae9ae729c9f3fd63c084c19047cb3c31d4044b
Deadpool2611200 <87938588+Deadpool2611200@users.noreply.github.com>
2022-07-13 20:49:38 +0400

    Add files via upload

---
+++ i3-gaps-deb/README.md
@@ -0,0 +1,28 @@
+# i3-gaps-deb
+Tool to create and install Debian packages of _i3-gaps_.
+
+## How does it work?
+It uses the newest stuff from [here](https://github.com/Airblader/i3),
+makes some modifications (only if necessary for the _building step_)
+and finally produces common Debian packages.
+
+## What's the purpose?
+_i3-gaps_ simply does not exist in Debian (so far).
+
+## How do I use it?
+    $ git clone git@github.com:maestrogerardo/i3-gaps-deb.git
+    $ cd i3-gaps-deb
+    $ ./i3-gaps-deb
+    $ # Follow the interactive prompts.
+
+## Works for what?
+  - Debian "stable" _and later_.
+  - Everything derived from above (e.g. Ubuntu).
+
+## Credits
+Thanks to [stapelberg](https://github.com/stapelberg) for creating
+[i3](http://i3wm.org/) and [Airblader](https://github.com/Airblader)
+for the _gaps part_  (...and of course to _anyone else_ contributing to _i3_).
+
+## Beer
+Hey, I'm [thirsty](http://gunbomber.org/donation.html) like hell... _;-)_
---
+++ i3-gaps-deb/bash-lib/gad-lib.bash
@@ -0,0 +1,100 @@
+#  gad-lib.bash is a small bash library providing some rudimentary stuff.
+#  Copyright (C) 2018  Gerhard A. Dittes
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Namespace: gad
+
+# Variables
+
+gad_LABEL="gad"
+
+# Functions
+
+gad_print()
+{
+	tput setaf 3 2>/dev/null || true
+	printf "[${gad_LABEL}] "
+	tput setaf 7 2>/dev/null || true
+	printf "%s" "${@}"
+	tput sgr0 2>/dev/null || true
+}
+
+gad_printLine()
+{
+	gad_print "${@}"; printf "\n"
+}
+
+gad_log()
+{
+	gad_printLine "${@}" >&2
+}
+
+gad_readAndContinue()
+{
+	gad_print "${@}"
+	read -p " [RETURN]"
+}
+
+gad_question()
+{
+	local result=1 # false
+	local question="${1}"
+	local default="n"
+
+	# ensure lowercase
+	if [ ${#} -eq 2 ] && [ "${2}" = "y" -o "${2}" = "Y" ]; then
+		default="y"
+	fi
+
+	gad_print "${question}"
+
+	if [ ${default} = "y" ]; then
+		printf " [Y/n]: "
+	else
+		printf " [y/N]: "
+	fi
+
+	read answer
+
+	# ensure lowercase
+	if [ "${answer}" = "Y" ]; then
+		answer="y"
+	elif [ "${answer}" = "N" ]; then
+		answer="n"
+	fi
+
+	if [ ${default} = "y" ]; then
+		if [ "${answer}" != "n" ]; then
+			result=0 # true
+		fi
+	else
+		if [ "${answer}" = "y" ]; then
+			result=0 # true
+		fi
+	fi
+
+	return ${result}
+}
+
+gad_logLine()
+{
+	local colums=$(tput cols)
+	local labelLength=${#gad_LABEL}
+	local extraLength=3
+	local length=$((${colums} - ${labelLength} - ${extraLength}))
+	local line="$(printf "%${length}s" | tr " " -)"
+
+	gad_log "${line}"
+}
